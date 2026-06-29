8f0b6b4aa4be57f7b500397aca5fbb6636b97b04
Elie-Wakim <eliewakim28@gmail.com>
2026-06-29 10:16:15 +0300

    Added the print format as fixtures

--- hospital_management/hooks.py
+++ hospital_management/hooks.py
@@ -247,3 +247,6 @@ export_python_type_annotations = True
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures=[
+    {"dt":"Print Format","filters":[["module","=","Hospital Management"]]}
+]
